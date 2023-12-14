c76430cc99f5a8415d610e92afd4494d191629c7
sawaovo <145239382+sawaovo@users.noreply.github.com>
2023-12-15 00:52:42 +0800

    Add files via upload

--- src/extraSettings.js
+++ src/extraSettings.js
@@ -37,7 +37,7 @@ let clickChangeFruit = false;
 // 广告链接：false 或为空字符串表示不会跳转到广告
 let adLink = false;
 
-// 修改网页标题：将 "合成大西瓜" 进行替换
+// 修改网页标题：将 "合成椎名尼" 进行替换
 document.getElementsByTagName("title")[0].innerText = '合成椎名尼';
 
 // 开启选分弹窗：将 false 改为 true
